64bef6f93f8ec4b2810523708fb71a87bde55283
poikilos <7557867+poikilos@users.noreply.github.com>
2021-06-25 00:40:28 -0400

    Add license information and 'show' subcommands.

--- ocremix-download-all.sh
+++ ocremix-download-all.sh
@@ -1,4 +1,54 @@
 #!/bin/bash
+
+
+SHOW_W="/tmp/ocremix-download-all-show_w.txt"
+if [ -f "$SHOW_W" ]; then
+    rm "$SHOW_W"
+fi
+
+DST="/dev/null"
+if [ "@$1" = "@show" ]; then
+    if [ "@$2" = "@c" ]; then
+        if [ -f "license.txt" ]; then
+            cat license.txt
+        else
+            echo "See license.txt in the ocremix-download-all directory"
+            echo "or at <https://github.com/poikilos/ocremix-download-all>."
+        fi
+        exit 0
+    fi
+    DST="$SHOW_W"
+fi
+cat > $DST <<END
+    ocremix-download-all syncs ocremix.org's library with a directory.
+    Copyright (C) 2010 Kurin on ocremix.org
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+END
+if [ -f "$SHOW_W" ]; then
+    cat "$SHOW_W"
+    rm "$SHOW_W"
+    exit 0
+else
+    cat <<END
+    ocremix-download-all  Copyright (C) 2010 Kurin on ocremix.org
+    This program comes with ABSOLUTELY NO WARRANTY; for details type 'show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type 'show c' for details.
+END
+fi
+
 FOLDER="OCRemix";
 START=1;
 END=3000;
